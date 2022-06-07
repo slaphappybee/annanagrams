2eb32028e6fc1c6c968699589464809090fc2cf3
Anna Bellec <32549757+slaphappybee@users.noreply.github.com>
2022-06-07 15:12:16 +0100

    Update header

--- src/header.tsx
+++ src/header.tsx
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                <h1>Annanagrams!</h1>
+                <h1>🍩 Annanagrams! 🍩</h1>
             </div>
         )
     }
